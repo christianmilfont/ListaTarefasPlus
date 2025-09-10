9ef32dac203c699c76af8c7ddb2fd524d21dbe0c
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-10 09:40:58 -0300

    removendo navigate do app

--- App.jsx
+++ App.jsx
@@ -1,31 +1,14 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ThemeProvider, useTheme } from './src/context/ThemeContext'; // seu provider
-import './src/services/i18n'; // importa configuração i18n
+import { ThemeProvider } from './src/context/ThemeContext';
+import './src/services/i18n'; // configuração i18n
 
-// Telas
-import FormScreen from './src/app/FormScreen';
-import AddTaskScreen from './src/app/AddTaskScreen';
-import LoginScreen from './src/app';
-import HomeScreen from './src/app/Home';
-
-const Stack = createNativeStackNavigator();
 const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <ThemeProvider>
       <QueryClientProvider client={queryClient}>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Cadastro" component={FormScreen} />
-            <Stack.Screen name="Adicionar Task" component={AddTaskScreen} />
-            <Stack.Screen name="Home" component={HomeScreen} />
-          </Stack.Navigator>
-        </NavigationContainer>
       </QueryClientProvider>
     </ThemeProvider>
   );
